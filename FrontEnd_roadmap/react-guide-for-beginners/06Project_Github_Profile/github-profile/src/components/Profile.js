bd65262c0f511ce8059382ddc7708a0bd2057f36
jinsanity07git <443605310@qq.com>
2019-10-16 20:09:45 -0500

    github profile  API

--- FrontEnd_roadmap/react-guide-for-beginners/06Project_Github_Profile/github-profile/src/components/Profile.js
+++ FrontEnd_roadmap/react-guide-for-beginners/06Project_Github_Profile/github-profile/src/components/Profile.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component } from 'react';
 
-function  Profile() {
-  return (
-    <div >
-  <p>Profile </p>
 
-    </div>
-  );
+
+class Profile extends Component{  
+
+    componentDidMount(){
+        let header = new Headers({"Content-Type": "application/json", "Authorization": "token eff71bf5c7b3e7231a3ee7386873696a219233f2"  })
+        fetch('https://api.github.com/users/jinsanity07git',{
+            method: 'GET',
+            headers: header
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            this.setState({
+                jsonList :json
+            });
+          })
+            .catch(error => {
+                console.log(error)
+            })
+
+    }
+render(){
+    return (
+        <div >
+    <p>Profile </p>
+
+        </div>
+    );
+    }
 }
-
 export default Profile;
